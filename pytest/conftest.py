cef94ea5d080dd3dfd494cdfb106485fdc01759f
Komisaryk <komisaryky@gmail.com>
2022-05-31 01:17:47 +0300

    3_6_6 Add browser_name=firefox

--- pytest/conftest.py
+++ pytest/conftest.py
@@ -6,13 +6,25 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 import math
 
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
 
 @pytest.fixture(scope="function")
-def browser():
+def browser(request):
     print("\nstart browser for test..")
     options = webdriver.ChromeOptions()
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    browser = webdriver.Chrome(options=options)
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     browser.implicitly_wait(10)
     yield browser
     print("\nquit browser..")
